2a369a521b1d9e08cb42731a19f9c4d53408202d
Juno <sojae0306@gmail.com>
2025-05-05 21:45:18 +0900

    BinarySearchTree 구현 및 테스트

---
+++ Part_06/BinarySearchTree/Test_BinarySearchTree.c
@@ -0,0 +1,62 @@
+#include "BinarySearchTree.h"`
+
+void PrintSearchResult(int SearchTarget, BSTNode *Result)
+{
+    if (Result != NULL)
+        printf("Found: %d\n", Result->Data);
+    else
+        printf("Not Found: %d\n", SearchTarget);
+}
+
+int main()
+{
+    // Create Nodes
+    BSTNode *Tree = BST_CreateNode(123);
+    BSTNode *Node = NULL;
+
+    // Insert Nodes
+    BST_InsertNode(Tree, BST_CreateNode(22));
+    BST_InsertNode(Tree, BST_CreateNode(9918));
+    BST_InsertNode(Tree, BST_CreateNode(424));
+    BST_InsertNode(Tree, BST_CreateNode(17));
+    BST_InsertNode(Tree, BST_CreateNode(3));
+
+    BST_InsertNode(Tree, BST_CreateNode(98));
+    BST_InsertNode(Tree, BST_CreateNode(34));
+
+    BST_InsertNode(Tree, BST_CreateNode(760));
+    BST_InsertNode(Tree, BST_CreateNode(317));
+    BST_InsertNode(Tree, BST_CreateNode(1));
+
+    int SearchTarget = 17;
+    Node = BST_SearchNode(Tree, SearchTarget);
+    PrintSearchResult(SearchTarget, Node);
+
+    SearchTarget = 117;
+    Node = BST_SearchNode(Tree, SearchTarget);
+    PrintSearchResult(SearchTarget, Node);
+
+    // Print Tree
+    BST_InorderPrintTree(Tree);
+    printf("\n");
+
+    // 특정 노드 삭제
+    printf("Removing 98...\n");
+    Node = BST_RemoveNode(Tree, NULL, 98);
+    BST_DestroyNode(Node);
+
+    BST_InorderPrintTree(Tree);
+    printf("\n");
+
+    // 새 노드 삽입
+    printf("Inserting 111...\n");
+
+    BST_InsertNode(Tree, BST_CreateNode(111));
+    BST_InorderPrintTree(Tree);
+    printf("\n");
+
+    // 트리 소멸
+    BST_DestroyTree(Tree);
+
+    return 0;
+}
